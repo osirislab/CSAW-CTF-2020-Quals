5045fad77028c797d06d92f511d789b0a4434a99
ex0dus-0x <ex0dus@codemuch.tech>
2020-08-22 16:04:15 -0400

    Add ez crypto challenge difib

---
+++ crypto/difib/README.md
@@ -0,0 +1,13 @@
+## difib
+
+__Category__: Crypto
+
+__Point Value__: 50-100
+
+TODO: add some more bogus and garbage panagramss that aren't perfect
+
+```
+Let's start off sweet and simple :)
+
+snbwmuotwodwvcywfgmruotoozaiwghlabvuzmfobhtywftopmtawyhifqgtsiowetrksrzgrztkfctxnrswnhxshylyehtatssukfvsnztyzlopsv
+```
---
+++ crypto/difib/solver.py
@@ -0,0 +1,60 @@
+"""
+solver.py
+
+    Uses bifid cipher to decrypt ciphertext from given perfect pangrams.
+"""
+
+import base64
+
+from secretpy import Bifid
+from secretpy import CryptMachine
+
+# bad characters to remove from the key
+REMOVE = [" ", ".", ",", "+", "-", ":", "@", "'", "!", "?", "j", "\n"]
+
+
+def main():
+
+    # parse out each verse
+    with open("verses", "r") as fd:
+        keystrings = fd.readlines()
+
+    # filter and cleanup into a 25-char key
+    keys = []
+    for key in keystrings:
+        key = key.lower()
+        for rm in REMOVE:
+            key = key.replace(rm, "")
+
+        # helps us catch characters we don't want
+        assert len(key) == 25
+        keys += [key]
+
+    # TODO: filter again once we add garbage to verses
+
+    # take each cleaned up key and encrypt using Bifid cipher
+    key = 5
+    cm = CryptMachine(Bifid(), key)
+
+    plaintext = u"just some unnecessary text that holds absolutely no meaning whatsoever and bears no significance to you in any way"
+    ciphertext = plaintext
+
+    # encrypt plaintext
+    for key in keys:
+        alphabet = [u'{}'.format(i) for i in list(key)]
+        cm.set_alphabet(alphabet)
+        ciphertext = cm.encrypt(ciphertext)
+
+    print("Ciphertext generated: {}".format(ciphertext))
+
+    # decrypt plaintext
+    final_plaintext = ciphertext
+    for key in reversed(keys):
+        alphabet = [u'{}'.format(i) for i in list(key)]
+        cm.set_alphabet(alphabet)
+        final_plaintext = cm.decrypt(final_plaintext)
+
+    print("Final Plaintext recovered: {}".format(final_plaintext))
+
+if __name__ == "__main__":
+    main()
